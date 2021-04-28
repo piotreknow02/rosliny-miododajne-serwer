779ff81e1a7aa8b62b0d4cb1cec36bb0b6e7098e
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-04-28 17:45:15 +0200

    Dodano nginx

--- router.ts
+++ router.ts
@@ -4,12 +4,12 @@ import PlantController from "./controllers/plantController.ts";
 const router: Router = new Router();
 const controller = new PlantController(Deno.env.get("MYSQL_SERVER_ADDR")!);
 
-router.post("/plants", async (ctx: RouterContext) => {
-    ctx.response.type = "application/json";
-    console.log((await ctx.request.body()).value);
-    const data = await controller.GetFromQuery(await ctx.request.body().value);
-    ctx.response.body = JSON.stringify(data);
-});
+// router.post("/plants", async (ctx: RouterContext) => {
+//     ctx.response.type = "application/json";
+//     console.log((await ctx.request.body()).value);
+//     const data = await controller.GetFromQuery(await ctx.request.body().value);
+//     ctx.response.body = JSON.stringify(data);
+// });
 
 router.get("/all-plants", async (ctx: RouterContext) => {
     ctx.response.type = "application/json";
