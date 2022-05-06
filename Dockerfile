d9d239ba77136d61b01e2c8ce6084639365f5368
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2022-05-06 16:38:51 +0200

    Aktualizacja kontenerów i wersji oak

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM hayd/ubuntu-deno:latest
+FROM denoland/deno:latest
 
 ADD . /home/project/
 WORKDIR /home/project/
-RUN deno upgrade
 CMD ["run", "--allow-net=0.0.0.0:3000,mysql:3306", "--allow-read=/", "--allow-env=MYSQL_SERVER_ADDR,MYSQL_ROOT_PASSWORD", "server.ts"]
 
 # docker build -t rosliny-server .
--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { Application, Router } from "https://deno.land/x/oak/mod.ts";
-export type { RouterContext } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
 export { Client } from "https://deno.land/x/mysql/mod.ts";
 export type { ExecuteResult } from "https://deno.land/x/mysql/src/connection.ts";
 export { config } from "https://deno.land/x/dotenv/mod.ts";
